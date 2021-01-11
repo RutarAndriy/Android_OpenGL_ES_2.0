e199ba3108da3351d79d0057cae669c7e1d8ffd6
RutarAndriy <RutarAndriy@gmail.com>
2021-01-11 15:57:59 +0200

    Project template generated

---
+++ app/src/main/java/com/rutar/opengl_es_20/OpenGL_ES_20.java
@@ -0,0 +1,14 @@
+package com.rutar.opengl_es_20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpenGL_ES_20 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

dfc1c03acbae2897250b05365470b8f1165bdafa
RutarAndriy <RutarAndriy@gmail.com>
2021-01-12 18:14:50 +0200

    Created examples list

--- app/src/main/java/com/rutar/opengl_es_20/OpenGL_ES_20.java
+++ app/src/main/java/com/rutar/opengl_es_20/OpenGL_ES_20.java
@@ -1,14 +1,125 @@
 package com.rutar.opengl_es_20;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.content.*;
 
-import android.os.Bundle;
+import android.annotation.*;
+import androidx.annotation.*;
+import androidx.appcompat.app.*;
+import androidx.recyclerview.widget.*;
 
 public class OpenGL_ES_20 extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+// ................................................................................................
+
+private final Object[][] examples = new Object[][] {};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+@Override
+protected void onCreate (Bundle bundle) {
+
+super.onCreate(bundle);
+setContentView(R.layout.activity_main);
+
+RecyclerView recycler_view = findViewById(R.id.examples_list);
+recycler_view.setLayoutManager(new LinearLayoutManager(this));
+recycler_view.setAdapter(new Recycler_View_Adapter(this, examples));
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Адаптер для списку пунктів меню
+
+private class Recycler_View_Adapter extends RecyclerView.Adapter<Recycler_View_Holder> {
+
+private final Object[][] examples;
+private final LayoutInflater inflater;
+
+// ................................................................................................
+
+public Recycler_View_Adapter (Context context, Object[][] data) {
+    this.inflater = LayoutInflater.from(context);
+    this.examples = data;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+@NonNull
+@Override
+public Recycler_View_Holder onCreateViewHolder (@NonNull ViewGroup parent, int view_type) {
+    View view = inflater.inflate(R.layout.recycler_item_view, parent, false);
+    return new Recycler_View_Holder(view);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+@Override
+public void onBindViewHolder (Recycler_View_Holder holder, int position) {
+
+String title = (String) examples[position][0];
+holder.title.setText(title);
+
+holder.start.setTag(position);
+holder.about.setTag(position);
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+@Override
+public int getItemCount() {
+    return examples.length;
+}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Клас-контейнер даних для пунктів меню
+
+private class Recycler_View_Holder extends RecyclerView.ViewHolder {
+
+public TextView title;
+public Button   start;
+public Button   about;
+
+// ................................................................................................
+
+public Recycler_View_Holder (View item) {
+
+super(item);
+title = item.findViewById(R.id.example_title);
+start = item.findViewById(R.id.example_start);
+about = item.findViewById(R.id.example_about);
+
+start.setOnClickListener(click_listener);
+about.setOnClickListener(click_listener);
+
+}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Прослуховувач пунктів меню
+
+@SuppressLint("NonConstantResourceId")
+private final View.OnClickListener click_listener = view -> {
+
+switch (view.getId()) {
+
+case R.id.example_start:
+
+    startActivity(new Intent(this, (Class<?>) examples[(int) view.getTag()][1]));
+    break;
+
+case R.id.example_about:
+
+    break;
+
+}
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Кінець класу OpenGL_ES_20
+
 }
